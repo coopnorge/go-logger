d59af52179364e689eb78afa26a553b3669c200f
Alfredo Suarez <alfredo.suarez.aceves@coop.no>
2024-06-04 15:10:26 +0200

    removing erros.go, as its not needed

--- errors.go
+++
@@ -1,6 +0,0 @@
-package logger
-
-import "errors"
-
-// ErrInvalidNameInEnv to be used if a name is not found in the available levels
-var ErrInvalidNameInEnv = errors.New("invalid name passed to available log levels")
