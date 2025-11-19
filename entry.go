62399b4c1fd9a223b1fdf4a92eac070b5c85436a
Stig-Rune Skansgård <stig-rune.skansgaard@coop.no>
2025-11-19 12:52:52 +0100

    make Log and Logf private (log and logf)

--- entry.go
+++ entry.go
@@ -55,73 +55,73 @@ func addCallerFields(logrusFields logrus.Fields, reportCaller bool) {
 
 // Info forwards a logging call in the (format, args) format
 func (e *Entry) Info(args ...interface{}) {
-	e.Log(LevelInfo, args...)
+	e.log(LevelInfo, args...)
 }
 
 // Infof forwards a logging call in the (format, args) format
 func (e *Entry) Infof(format string, args ...interface{}) {
-	e.Logf(LevelInfo, format, args...)
+	e.logf(LevelInfo, format, args...)
 }
 
 // Error forwards an error logging call
 func (e *Entry) Error(args ...interface{}) {
-	e.Log(LevelError, args...)
+	e.log(LevelError, args...)
 }
 
 // Errorf forwards an error logging call
 func (e *Entry) Errorf(format string, args ...interface{}) {
-	e.Logf(LevelError, format, args...)
+	e.logf(LevelError, format, args...)
 }
 
 // Debug forwards a debugging logging call
 func (e *Entry) Debug(args ...interface{}) {
-	e.Log(LevelDebug, args...)
+	e.log(LevelDebug, args...)
 }
 
 // Debugf forwards a debugging logging call
 func (e *Entry) Debugf(format string, args ...interface{}) {
-	e.Logf(LevelDebug, format, args...)
+	e.logf(LevelDebug, format, args...)
 }
 
 // Warn forwards a warning logging call
 func (e *Entry) Warn(args ...interface{}) {
-	e.Log(LevelWarn, args...)
+	e.log(LevelWarn, args...)
 }
 
 // Warnf forwards a warning logging call
 func (e *Entry) Warnf(format string, args ...interface{}) {
-	e.Logf(LevelWarn, format, args...)
+	e.logf(LevelWarn, format, args...)
 }
 
 // Fatal forwards a fatal logging call
 func (e *Entry) Fatal(args ...interface{}) {
-	e.Log(LevelFatal, args...)
+	e.log(LevelFatal, args...)
 }
 
 // Fatalf forwards a fatal logging call
 func (e *Entry) Fatalf(format string, args ...interface{}) {
-	e.Logf(LevelFatal, format, args...)
+	e.logf(LevelFatal, format, args...)
 }
 
-// Logf forwards a logging call
-func (e *Entry) Logf(level Level, format string, args ...any) {
+// logf forwards a logging call
+func (e *Entry) logf(level Level, format string, args ...any) {
 	logrusFields := logrus.Fields(e.fields)
 	addCallerFields(logrusFields, e.logger.reportCaller)
 	e.logger.logrusLogger.WithContext(e.context).WithTime(e.logger.now()).WithFields(logrusFields).Logf(mapLevelToLogrusLevel(level), format, args...)
 
-	// This ensures that logging with level Fatal results in Exit regardless if using .Fatalf or .Logf(LevelFatal, ...)
+	// This ensures that logging with level Fatal results in Exit regardless if using .Fatalf or .logf(LevelFatal, ...)
 	if level == LevelFatal {
 		e.logger.logrusLogger.Exit(1)
 	}
 }
 
-// Log forwards a logging call
-func (e *Entry) Log(level Level, args ...any) {
+// log forwards a logging call
+func (e *Entry) log(level Level, args ...any) {
 	logrusFields := logrus.Fields(e.fields)
 	addCallerFields(logrusFields, e.logger.reportCaller)
 	e.logger.logrusLogger.WithContext(e.context).WithTime(e.logger.now()).WithFields(logrusFields).Log(mapLevelToLogrusLevel(level), args...)
 
-	// This ensures that logging with level Fatal results in Exit regardless if using .Fatal or .Log(LevelFatal, ...)
+	// This ensures that logging with level Fatal results in Exit regardless if using .Fatal or .log(LevelFatal, ...)
 	if level == LevelFatal {
 		e.logger.logrusLogger.Exit(1)
 	}
